bbcc1be7ac58d56b05d9d33a329a340f544cd99c
ozanturkes <ozan.turkes@gmail.com>
2020-06-30 12:27:16 -0500

    some_edits

--- trash/tr.py
+++
@@ -1,7 +0,0 @@
-
-
-a = '001'
-b = '05000'
-
-website = 'https://cogcc.state.co.us/production/?&apiCounty='+a+'&apiSequence='+b+'&APIWB=00&Year=All'
-print(website)
--- trash/trh.py
+++
@@ -1,6 +0,0 @@
-
-import geopandas as gpd
-data = gpd.read_file('/Users/ozanturkes/coding/GitHub/prod_data/data/WELLS_SHP/Wells.shp')
-
-print(data)
-
