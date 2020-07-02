1d3df317af07d6404a60d54f0d4e9dca6b946f2c
ozntur <ozan.turkes@gmail.com>
2020-07-02 09:50:23 -0500

    hist

---
+++ code/histogram.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import colors
+from matplotlib.ticker import PercentFormatter
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
